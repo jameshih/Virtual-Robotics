6184187b55dda6ddba863c9938c69bd2034f24a9
jimmyadg <jimmyadg@gmail.com>
2015-12-09 16:59:51 -0500

    robot_v1

---
+++ robot_v1/debug.ino
@@ -0,0 +1,10 @@
+void debug(){
+  Serial.print("Servo Degree= \t");
+  Serial.print(pos);
+  Serial.print("\tDistance= \t");
+  Serial.print(distance);
+  Serial.print("\tTimer= \t");
+  Serial.print(timer);
+  Serial.println();
+}
+
